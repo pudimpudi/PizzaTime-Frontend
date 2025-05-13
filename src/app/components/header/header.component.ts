90da16331a640dbf0436b72b837771bfb7005dce
pudimpudi <damatodutragabrielle@gmail.com>
2025-05-12 21:36:57 -0300

    Header quase pronto

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  standalone: false,
+  standalone: true,
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-
+  
 }
